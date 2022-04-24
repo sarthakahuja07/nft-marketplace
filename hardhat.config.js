487283259835b564e6884ebc4a43638667cfc276
sarthak <sarthak.ahuja0007@gmail.com>
2022-04-24 06:56:33 +0530

    hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+	defaultNetwork: "hardhat",
+	networks: {
+		hardhat: {
+			chainId: 1337
+		}
+		/*
+    mumbai: {
+      // Infura
+      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [process.env.privateKey]
+    },
+    matic: {
+      // Infura
+      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts: [process.env.privateKey]
+    }
+    */
+	},
+	solidity: {
+		version: "0.8.4",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200
+			}
+		}
+	}
+};
